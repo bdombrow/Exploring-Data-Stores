b6d4354fa297b5891cf3e446a432dd09413c16cb
Brent Dombrowski <bdombrow@cecs.pdx.edu>
2011-12-25 21:02:15 -0800

    Testing multi put. It looks like it will be better to have the nodes and edges in separate tables. The column family distinction is to as great as Cassandra.

--- HBase/Loader/Loader/src/Loader/Loader.java
+++ HBase/Loader/Loader/src/Loader/Loader.java
@@ -1,15 +1,12 @@
 package Loader;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.Get;
-import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.client.ResultScanner;
-import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 public class Loader {
@@ -19,17 +16,50 @@ public class Loader {
         Configuration config = HBaseConfiguration.create();
         config.set("hbase.zookeeper.quorum", "10.9.73.25");
 
-        // Set the table we are using
-        HTable table = new HTable(config, "graph");
+        // Set the nodes we are using
+        HTable nodes = new HTable(config, "graph");
 
         // Create a new row and populate it's data
-        Put p = new Put(Bytes.toBytes("1"));      // Set the row first
-        p.add(Bytes.toBytes("nodes"), Bytes.toBytes("name"), Bytes.toBytes("Brent"));  // Column Family, Column Name, Value
-        p.add(Bytes.toBytes("nodes"), Bytes.toBytes("gender"), Bytes.toBytes("Male"));
-        p.add(Bytes.toBytes("nodes"), Bytes.toBytes("occupation"), Bytes.toBytes("Student"));
+        Put p1 = new Put(Bytes.toBytes("1"));      // Set the row first
+        p1.add(Bytes.toBytes("nodes"), Bytes.toBytes("name"), Bytes.toBytes("Brent"));  // Column Family, Column Name, Value
+        p1.add(Bytes.toBytes("nodes"), Bytes.toBytes("gender"), Bytes.toBytes("Male"));
+        p1.add(Bytes.toBytes("nodes"), Bytes.toBytes("occupation"), Bytes.toBytes("Masters Student"));
 
         // Put the new row into HBase
-        table.put(p);
+        //nodes.put(p1);
+        
+        // Create a second row to test multiple put
+        Put p2 = new Put(Bytes.toBytes("2"));
+        p2.add(Bytes.toBytes("nodes"), Bytes.toBytes("name"), Bytes.toBytes("Steve"));
+        p2.add(Bytes.toBytes("nodes"), Bytes.toBytes("gender"), Bytes.toBytes("Male"));
+        p2.add(Bytes.toBytes("nodes"), Bytes.toBytes("occupation"), Bytes.toBytes("Architect"));
+        
+        // Multiple Put
+        List mput = new ArrayList<Put>();
+        mput.add(p1);
+        mput.add(p2);
+
+        //nodes.put(mput);
+        
+        // Add an edge and see what happens
+        //HTable edges = new HTable(config, "graph");
+        Put p3 = new Put(Bytes.toBytes("1"));
+        p3.add(Bytes.toBytes("edges"), Bytes.toBytes("relation"), Bytes.toBytes("Brother"));
+        p3.add(Bytes.toBytes("edges"), Bytes.toBytes("start"), Bytes.toBytes("1"));
+        p3.add(Bytes.toBytes("edges"), Bytes.toBytes("end"), Bytes.toBytes("2"));
+
+        //edges.put(p3);
+        
+        // Column families do not appear to be as distinct as Cassandra
+        mput.add(p3);
+        nodes.put(mput);
+
+        // Clean up
+        nodes.flushCommits();
+        //edges.flushCommits();
+        nodes.close();
+        //edges.close();
+        
 
     }
 }
