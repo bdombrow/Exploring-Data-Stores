1d17505e880216d8514af971b0128781b6bc0ac8
Brent Dombrowski <bdombrow@cecs.pdx.edu>
2012-01-03 10:34:36 -0800

    Added support for undirected graphs in friends of fiends.

--- HBase/FoF/src/FoF/FoF.java
+++ HBase/FoF/src/FoF/FoF.java
@@ -7,6 +7,7 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.filter.CompareFilter;
+import org.apache.hadoop.hbase.filter.FilterList;
 import org.apache.hadoop.hbase.filter.SingleColumnValueFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 
@@ -18,7 +19,6 @@ import java.util.TreeSet;
  * Finds the friends of friends for a given node.
  *
  * Starting node, table, and column family are all hard coded.
- * This is set up for a directed graph.
  *
  * Created by IntelliJ IDEA.
  * User: brent
@@ -27,11 +27,24 @@ import java.util.TreeSet;
  * To change this template use File | Settings | File Templates.
  */
 public class FoF {
+
+    // Set the graph type
+    private static final boolean UNDIRECTED = false;
+
+    /**
+     * main routine.
+     *
+     * Gets the friends of friends for the hard coded node.
+     *
+     * @param args Not used.
+     * @throws Exception Passed up from HBase. No exception handling has been implemented.
+     */
+    
     public static void main(String[] args) throws Exception {
         TreeSet<String> friends = new TreeSet<String>();
         TreeSet<String> friendsofFriends = new TreeSet<String>();
 
-        friends = getFriends("1");
+        friends.addAll(getFriends("1"));
 
         for (String friend : friends) {
             friendsofFriends.addAll(getFriends(friend));
@@ -46,28 +59,48 @@ public class FoF {
      *
      * Scans the table for the friends of a given node.
      *
-     * @param start Starting node
+     * @param node Starting node
      * @return TreeSet of Friends
      * @throws Exception
      */
 
-    private static TreeSet<String> getFriends(String start) throws Exception {
+    private static TreeSet<String> getFriends(String node) throws Exception {
         // Configure HBase connection
         Configuration config = HBaseConfiguration.create();
         config.set("hbase.zookeeper.quorum", "10.9.73.25");
 
         // Set the table
         HTable table = new HTable(config, "graph");
+        
+        String cf;
+        
+        if (UNDIRECTED) {
+            cf = "wedges";
+        } else {
+            cf = "uedges";
+        }
 
-        // Filter results by Start qualifier in uedges column family (case sensitive)
-        SingleColumnValueFilter singleColumnValueFilter;
-        singleColumnValueFilter = new SingleColumnValueFilter(Bytes.toBytes("uedges"), Bytes.toBytes("Start"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(start));
+        // Filter results by Start qualifier (case sensitive)
+        SingleColumnValueFilter startFilter;
+        startFilter = new SingleColumnValueFilter(Bytes.toBytes(cf), Bytes.toBytes("Start"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(node));
+
+        // Filter results by End qualifier (case sensitive)
+        SingleColumnValueFilter endFilter;
+        endFilter = new SingleColumnValueFilter(Bytes.toBytes(cf), Bytes.toBytes("End"), CompareFilter.CompareOp.EQUAL, Bytes.toBytes(node));
+
+        // Set up the filter list
+        FilterList filters;
+        filters = new FilterList(FilterList.Operator.MUST_PASS_ONE);
+        filters.addFilter(startFilter);
+        if (UNDIRECTED) {
+            filters.addFilter(endFilter);
+        }
 
         // Create a new scan
         Scan s = new Scan();
-        s.addColumn(Bytes.toBytes("uedges"),Bytes.toBytes("Start")); // Start is needed for the filter
-        s.addColumn(Bytes.toBytes("uedges"), Bytes.toBytes("End"));
-        s.setFilter(singleColumnValueFilter);
+        s.addColumn(Bytes.toBytes(cf),Bytes.toBytes("Start")); // Needed for filter and results
+        s.addColumn(Bytes.toBytes(cf), Bytes.toBytes("End")); // Needed for filter and results
+        s.setFilter(filters);
 
         // Get the results of the scan
         ResultScanner results = table.getScanner(s);
@@ -76,9 +109,14 @@ public class FoF {
 
         // Add the results of the scan to the friends set
         for (Result result : results) {
-            friends.add(
-                    Bytes.toString(result.getValue(Bytes.toBytes("uedges"), Bytes.toBytes("End")))
-            );
+            // Results are jumbled in the undirected case
+            String startNode = Bytes.toString(result.getValue(Bytes.toBytes(cf), Bytes.toBytes("Start")));
+            String endNode = Bytes.toString(result.getValue(Bytes.toBytes(cf), Bytes.toBytes("End")));
+            if (node.contentEquals(startNode)) {
+                friends.add(endNode);
+            } else {
+                friends.add(startNode);
+            }
         }
 
         // Clean up
